d965e46c4046a520681471e83c00de6738f4c6d3
jv8ai <coreymprice@gmail.com>
2025-09-04 13:51:28 -0600

    Update Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { createPageUrl } from "@/utils";
+import { createPageUrl } from "@/utils/index";
 import { User } from "@/api/entities";
 import { Button } from "@/components/ui/button";
 import {
